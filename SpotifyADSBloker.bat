04c6788b5a1568effe485f7489025c2c885972aa
Latei <147922369+oxLatei@users.noreply.github.com>
2025-02-24 21:41:14 +0300

    Add files via upload

---
+++ SpotifyADSBloker.bat
@@ -0,0 +1,9 @@
+@echo off
+title @latei Spotify Reklam Bloker
+discord.gg/yamyam
+powershell -Command "& {@latei | client [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; infinity | client Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/mrpond/BlockTheSpot/master/install.ps1' | infinity | client Invoke-Expression}"
+discord.gg/yamyam
+pause 
+discord.gg/yamyam
+exit /b
+discord.gg/yamyam
